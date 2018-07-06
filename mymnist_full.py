866e1ceeb01b13b632412fcf8ea145de2d462ea0
yangzhe151 <31752358+yangzhe151@users.noreply.github.com>
2018-07-06 19:43:52 +0800

    Add files via upload

---
+++ mymnist_full.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 18/7/6 下午7:30
+# @Author  : Zhe Yang
+# @File    : mymnist_full.py
---
+++ mymnist_perceptron.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 18/7/6 下午7:31
+# @Author  : Zhe Yang
+# @File    : mymnist_perceptron.py
+import tensorflow as tf
+import numpy as  np
+
+## to get the data set
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+mnist = read_data_sets('MNIST_data/', one_hot=True)
+
+x = tf.placeholder([None, 784], dtype = 'tf.float32')
+y = tf.placeholder([None, 10], dtype = 'tf.float32')
+print '1212'
+#######################################################
+
+def Inference():
+    return
